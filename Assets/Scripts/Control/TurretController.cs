548418a831f71a11c16ae79126a8070b46ef3b8a
johhaanndev <71643284+johhaanndev@users.noreply.github.com>
2022-08-17 21:57:28 +0200

    added project

---
+++ Assets/Scripts/Control/TurretController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Control
+{
+    public class TurretController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            InteractWithAim();
+        }
+
+        private void InteractWithAim()
+        {
+            GetComponent<TurretAim>().StartAiming();
+        }
+    }
+}
---
+++ Assets/Scripts/Movement/TurretAim.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Control
+{
+    public class TurretAim : MonoBehaviour
+    {
+        [SerializeField] GameObject pivot;
+
+        private List<GameObject> enemies = new List<GameObject>();
+        private Transform target;
+
+        void Update()
+        {
+            Debug.Log($"Enemies in range: {enemies.Count}");
+        }
+
+        public void StartAiming()
+        {
+            if (enemies.Count == 0)
+            {
+                return;
+            }
+
+            var closestDistance = 50f;
+            foreach(var enemy in enemies)
+            {
+                if ((enemy.transform.position - transform.position).magnitude < closestDistance)
+                {
+                    closestDistance = (enemy.transform.position - transform.position).magnitude;
+                    target = enemy.transform;
+                }
+            }
+
+            RotateBase(target.position);
+            RotatePivot(target);
+        }
+
+        private void RotateBase(Vector3 target)
+        {
+            var lookPos = target - transform.position;
+            lookPos.y = 0;
+            var rotation = Quaternion.LookRotation(lookPos);
+            transform.rotation = rotation;
+        }
+
+        private void RotatePivot(Transform target)
+        {
+            pivot.transform.LookAt(target);
+            Mathf.Clamp(pivot.transform.rotation.x, 0, 0.35f);
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Enemy"))
+            {
+                enemies.Add(other.gameObject);
+            }
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Enemy"))
+            {
+                enemies.Remove(other.gameObject);
+            }
+        }
+    }
+}
