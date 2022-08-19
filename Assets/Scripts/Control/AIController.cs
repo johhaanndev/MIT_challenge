9806b1353602a89a7a860d6320f2e90863fb895a
johhaanndev <71643284+johhaanndev@users.noreply.github.com>
2022-08-19 17:01:12 +0200

    Enemies pursue closest turret

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -1,18 +1,80 @@
-﻿using System.Collections;
+﻿using Game.Core;
+using Game.Movement;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class AIController : MonoBehaviour
+namespace Game.Control
 {
-    // Start is called before the first frame update
-    void Start()
+    public class AIController : MonoBehaviour
     {
-        
-    }
+        [SerializeField] float attackRange;
+        [Range(0, 1)]
+        [SerializeField] float speedFraction = 0.2f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private EnemyMover mover;
+        private Health health; 
+
+        private List<GameObject> turrets = new List<GameObject>();
+        private GameObject target;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            mover = GetComponent<EnemyMover>();
+            health = GetComponent<Health>();
+            turrets = GameObject.FindGameObjectsWithTag("Player").ToList();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (health.IsDead())
+                return;
+
+            if (turrets.Count == 0)
+                return;
+
+            if (!InAttackRangeOfTarget())
+                PursueBehaviour();
+
+            AttackBehaviour();
+        }
+
+        private void AttackBehaviour()
+        {
+            Debug.Log($"{gameObject.name} Attacking: {target.name}");
+        }
+
+        private void PursueBehaviour()
+        {
+            Debug.Log($"Pursuing turret: {target.name}");
+            mover.StartMoveAction(target.transform.position, speedFraction);
+
+        }
+
+        private GameObject GetClosestTurret(List<GameObject> turrets)
+        {
+            float minDistance = Mathf.Infinity;
+            foreach (var turret in turrets)
+            {
+                if (Vector3.Distance(turret.transform.position, transform.position) < minDistance)
+                {
+                    minDistance = Vector3.Distance(turret.transform.position, transform.position);
+                    target = turret;
+                }
+            }
+
+            return target;
+        }
+
+        private bool InAttackRangeOfTarget()
+        {
+            target = GetClosestTurret(turrets);
+            float distanceToTarget = Vector3.Distance(target.transform.position, transform.position);
+            return distanceToTarget <= attackRange;
+        }
     }
 }
---
+++ Assets/Scripts/Movement/EnemyMover.cs
@@ -0,0 +1,51 @@
+﻿using Game.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Game.Movement
+{
+
+    public class EnemyMover : MonoBehaviour, IAction
+    {
+        [SerializeField] float maxSpeed = 6f;
+
+        private Transform target;
+        private NavMeshAgent agent;
+        private Health health;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            health = GetComponent<Health>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            agent.enabled = !health.IsDead();
+            //UpdateAnimator();
+        }
+
+        public void StartMoveAction(Vector3 destination, float speedFraction)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            MoveTo(destination, speedFraction);
+        }
+
+        public void MoveTo(Vector3 destination, float speedFraction)
+        {
+            agent.destination = destination;
+            agent.speed = maxSpeed * Mathf.Clamp01(speedFraction);
+            agent.isStopped = false;
+        }
+
+        public void Cancel()
+        {
+            agent.isStopped = true;
+        }
+    }
+
+}
