5cd3a33a3841f478e18e56b942cc6210f415513f
johhaanndev <71643284+johhaanndev@users.noreply.github.com>
2022-08-22 12:11:40 +0200

    Added joystick pack and basic camera movement

---
+++ Assets/Scripts/Control/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using Game.Movement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Control
+{
+
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] Joystick joystick;
+
+        private CameraMover mover;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            mover = GetComponent<CameraMover>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var hor = joystick.Horizontal;
+            var ver = joystick.Vertical;
+
+            if (Mathf.Abs(hor) > 0.1f || Mathf.Abs(ver) > 0.1f)
+            {
+                MovementBehaviour(hor, ver);
+            }
+        }
+
+        private void MovementBehaviour(float hor, float ver)
+        {
+            mover.Move(hor, ver);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Movement/CameraMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Movement
+{
+    public class CameraMover : MonoBehaviour
+    {
+        [SerializeField] float speedFraction = 1;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void Move(float x, float z)
+        {
+            transform.position += new Vector3(x, 0, z) * speedFraction;
+        }
+    }
+}
