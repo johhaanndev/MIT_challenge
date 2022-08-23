7ca97a94c7e5aac6c23665b1cf257b6dd05e40d1
johhaanndev <71643284+johhaanndev@users.noreply.github.com>
2022-08-23 16:49:51 +0200

    Creation of mine controller and fighter

---
+++ Assets/Scripts/Control/MineController.cs
@@ -0,0 +1,29 @@
+﻿using Game.Fight;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Control
+{
+    public class MineController : MonoBehaviour
+    {
+        private MineFighter fighter;
+
+        private void Start()
+        {
+            fighter = GetComponent<MineFighter>();
+        }
+        private void ActivateMine()
+        {
+            fighter.Activation();
+        }
+
+        public void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Enemy"))
+            {
+                ActivateMine();
+            }
+        }
+    }
+}
