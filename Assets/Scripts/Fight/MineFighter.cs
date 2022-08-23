2ad34fa7914be57b7f88760f4d9e073093ddb120
johhaanndev <71643284+johhaanndev@users.noreply.github.com>
2022-08-23 16:57:51 +0200

    Mines creation (#6)
    * Creation of mine controller and fighter
    
    * Added parameters to mine fighter

---
+++ Assets/Scripts/Fight/MineFighter.cs
@@ -0,0 +1,34 @@
+﻿using Game.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Fight
+{
+    public class MineFighter : MonoBehaviour
+    {
+        [SerializeField] float countdown = 0;
+        [SerializeField] float explosionRange = 2;
+        [SerializeField] float mineDamage = 30;
+        [SerializeField] LayerMask enemiesLayer;
+
+        public void Activation()
+        {
+            StartCoroutine(Explode());
+        }
+
+        public IEnumerator Explode()
+        {
+            yield return new WaitForSeconds(countdown);
+            Debug.Log("BOOM");
+            var enemiesInRange = Physics.OverlapSphere(transform.position, explosionRange, enemiesLayer);
+
+            foreach (var enemy in enemiesInRange)
+            {
+                enemy.GetComponent<Health>().TakeDamage(mineDamage);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
