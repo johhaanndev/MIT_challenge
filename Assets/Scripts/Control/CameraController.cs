d1b3d5ff027d525fbdb261b167f87b5d020e17fa
johhaanndev <71643284+johhaanndev@users.noreply.github.com>
2022-08-25 11:32:25 +0200

    changed joystick on FixedUpdate

--- Assets/Scripts/Control/CameraController.cs
+++ Assets/Scripts/Control/CameraController.cs
@@ -25,7 +25,7 @@ namespace Game.Control
         }
 
         // Update is called once per frame
-        void Update()
+        void FixedUpdate()
         {
             if (Input.touchCount == 2)
             {
